4920859b45e51b9d32ec12ae9db22ee80382c24b
JOSEPH <lena.boya26@gmail.com>
2023-05-11 12:52:28 +0200

    dalle

--- src/page/CreatePost.jsx
+++ src/page/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://dalle-sxs9.onrender.com/api/v1/post', {
+        const response = await fetch('https://dalle-sxs9.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
